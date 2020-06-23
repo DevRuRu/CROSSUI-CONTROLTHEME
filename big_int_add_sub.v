76f8c611221fb00b73f38383fca9d0123cd1e4dd
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-06-23 17:48:42 +0000

    bump to version 0.2.1

--- big_int_add_sub.v
+++ big_int_add_sub.v
@@ -1,2 +1,16 @@
 
 module biginteger
+
+const (
+	two_pow_32         = 1 << 32
+	two_pow_32_minus_1 = (1 << 32) - 1
+)
+
+pub fn (big BigInteger) negative() BigInteger {
+	if big.sign == .zero {
+		return big
+	}
+
+	return BigInteger{
+		bits: big.bits
+		sign: if big.sign == BigIntegerSign.positive {
