1e808aecc1f71ea808b68ff468b27378d4e71187
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-07-01 12:21:30 +0000

    Bump version: 1.0.4 → 1.0.5

--- big_int_add_sub.v
+++ big_int_add_sub.v
@@ -29,3 +29,17 @@ pub fn add(a BigInteger, b BigInteger) BigInteger {
 	if a.sign == .zero {
 		return b
 	} else if b.sign == .zero {
+		return a
+	} else if a.sign == b.sign {
+		mut bits := []u32{len: 0}
+		if a.bits.len > b.bits.len {
+			return b + a
+		} else {
+			bits = add_a_b_length_asc(a.bits, b.bits)
+		}
+
+		return BigInteger{
+			sign: a.sign
+			bits: bits
+		}
+	} else {
