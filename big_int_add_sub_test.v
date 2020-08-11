b8fc630a315f63ce95ab7060009c53139d3831f3
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-08-11 16:12:03 +0000

    fixing lint issues

--- big_int_add_sub_test.v
+++ big_int_add_sub_test.v
@@ -20,3 +20,11 @@ fn test_add_3() {
 	b := from_str('-9683495887') or { panic('') }
 
 	assert '${b + a}' == '-1345656464'
+}
+
+fn test_add_4() {
+	a := from_str('-8337839423') or { panic('') }
+	b := from_str('-9683495887') or { panic('') }
+
+	assert '${b + a}' == '-18021335310'
+}
