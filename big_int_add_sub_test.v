3fb74b6397785c062beb407f7b08cdae03013b98
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-08-11 06:49:52 +0000

    Deleted old files.

--- big_int_add_sub_test.v
+++ big_int_add_sub_test.v
@@ -4,3 +4,19 @@ module biginteger
 fn test_add_1() {
 	a := from_str('8337839423') or { panic('') }
 	b := from_str('9683495887') or { panic('') }
+
+	assert '${b + a}' == '18021335310'
+}
+
+fn test_add_2() {
+	a := from_str('-8337839423') or { panic('') }
+	b := from_str('9683495887') or { panic('') }
+
+	assert '${b + a}' == '1345656464'
+}
+
+fn test_add_3() {
+	a := from_str('8337839423') or { panic('') }
+	b := from_str('-9683495887') or { panic('') }
+
+	assert '${b + a}' == '-1345656464'
