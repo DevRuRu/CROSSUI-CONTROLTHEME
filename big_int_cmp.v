7e4700e7d6890ab79a70a8789d50d29996974241
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-08-25 07:22:52 +0000

    Update show_detail.styl

--- big_int_cmp.v
+++ big_int_cmp.v
@@ -1,2 +1,13 @@
 
 module biginteger
+
+pub fn (a BigInteger) == (b BigInteger) bool {
+	if a.sign != b.sign || a.bits.len != b.bits.len {
+		return false
+	} else {
+		for i := 0; i < a.bits.len; i++ {
+			if a.bits[i] != b.bits[i] {
+				return false
+			}
+		}
+	}
