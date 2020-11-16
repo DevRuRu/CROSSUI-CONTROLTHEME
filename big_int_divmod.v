1b11861e3332e6a652753c2f84d65e9891ff768b
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2020-11-16 09:32:34 +0000

    build: Node.js@6.9

--- big_int_divmod.v
+++ big_int_divmod.v
@@ -4,3 +4,8 @@ module biginteger
 import math.bits
 
 pub fn (a BigInteger) / (b BigInteger) BigInteger {
+	quotient, _ := div_mod(a, b) or { return zero }
+	return quotient
+}
+
+pub fn (a BigInteger) % (b BigInteger) BigInteger {
