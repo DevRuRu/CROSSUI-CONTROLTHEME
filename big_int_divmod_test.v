1df1c0903ef06155ca5d1e5853296955ece34f2b
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-01-03 15:43:07 +0000

    ext4: don't save the error information if the block device is read-only

--- big_int_divmod_test.v
+++ big_int_divmod_test.v
@@ -4,4 +4,4 @@ fn test_divide_mod_big() {
 	a := from_str('987654312345678901234567890') or { panic('') }
 	b := from_str('98765432109876543210') or { panic('') }
 
-	assert '${a / 
+	assert '${a / b}' == '99999
