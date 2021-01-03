785b1867019ba549af23642dc2caa145527f8d98
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-01-03 18:27:02 +0000

    Better

--- big_int_divmod_test.v
+++ big_int_divmod_test.v
@@ -4,4 +4,5 @@ fn test_divide_mod_big() {
 	a := from_str('987654312345678901234567890') or { panic('') }
 	b := from_str('98765432109876543210') or { panic('') }
 
-	assert '${a / b}' == '99999
+	assert '${a / b}' == '9999999'
+	ass
