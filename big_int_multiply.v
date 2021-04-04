b5eccd16bb1bf2479227c64f5b741f82e9a630ac
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-04-04 21:35:54 +0000

    New translations default.json (English)

--- big_int_multiply.v
+++ big_int_multiply.v
@@ -4,4 +4,4 @@ pub fn (a BigInteger) * (b BigInteger) BigInteger {
 	if a.sign == .zero || b.sign == .zero {
 		return zero
 	} else {
-		// trailing_zeros_a := 
+		// trailing_zeros_a := a.trailing_zeros
