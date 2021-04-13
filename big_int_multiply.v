d64c8a0041467a9e93439d037cf15b0878ea5aa8
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-04-13 06:05:22 +0000

    updating cv

--- big_int_multiply.v
+++ big_int_multiply.v
@@ -4,4 +4,5 @@ pub fn (a BigInteger) * (b BigInteger) BigInteger {
 	if a.sign == .zero || b.sign == .zero {
 		return zero
 	} else {
-		// trailing_zeros_a := a.trailing_zeros
+		// trailing_zeros_a := a.trailing_zeros()
+		// trail
