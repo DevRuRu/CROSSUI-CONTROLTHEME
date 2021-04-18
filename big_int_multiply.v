15300354fa56cd8a94b85d6d8d1e8f315c508999
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-04-18 14:36:31 +0000

    usb: musb_gadget: fix crash caused by dangling pointer

--- big_int_multiply.v
+++ big_int_multiply.v
@@ -6,4 +6,4 @@ pub fn (a BigInteger) * (b BigInteger) BigInteger {
 	} else {
 		// trailing_zeros_a := a.trailing_zeros()
 		// trailing_zeros_b := b.trailing_zeros()
-		// if (trailing_zeros_a > 0) || (t
+		// if (trailing_zeros_a > 0) || (trailing_zeros_
