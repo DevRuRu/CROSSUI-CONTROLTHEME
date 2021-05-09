8453bc87a51cf43698beed237b3fedb681975cba
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-05-09 10:50:32 +0000

    White space fixes.

--- big_int_multiply.v
+++ big_int_multiply.v
@@ -9,3 +9,6 @@ pub fn (a BigInteger) * (b BigInteger) BigInteger {
 		// if (trailing_zeros_a > 0) || (trailing_zeros_b > 0) {
 		// 	trailing_zeros := trailing_zeros_a + trailing_zeros_b
 		// 	return (a.rshift(trailing_zeros_a) * b.rshift(trailing_zeros_b)).lshift(trailing_zeros)
+		// }
+
+	
