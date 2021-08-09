aa5f2486f8c46c7d7a07c06723c60f4a0db2e2e7
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-08-09 14:49:00 +0000

    clk: versatile: off by one in clk_sp810_timerclken_of_get()

--- big_int_multiply_test.v
+++ big_int_multiply_test.v
@@ -21,3 +21,14 @@ fn test_add_3() {
 
 	assert '${b * a}' == '-80739433759086953201'
 }
+
+fn test_add_4() {
+	a := from_str('0') or { panic('') }
+	b := from_str('9683495887') or { panic('') }
+
+	assert '${b * a}' == '0'
+}
+
+fn test_add_5() {
+	a := from_str('8337839423') or { panic('') }
+	b := from_str('0') or { panic('') }
