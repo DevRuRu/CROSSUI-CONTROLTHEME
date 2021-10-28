5ccb5a4978c916e21d3eb0e05a6275ec9343d090
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2021-10-28 22:07:01 +0000

    deleted extra files

--- big_int_num_from.v
+++ big_int_num_from.v
@@ -48,4 +48,6 @@ fn from_u64_and_sign(i u64, sign BigIntegerSign) BigInteger {
 
 	abs := i
 	abs_top := u32(abs >> 32)
-	bits := if abs_top == 0 { [u32(abs)] } else { [u32(abs), abs_top
+	bits := if abs_top == 0 { [u32(abs)] } else { [u32(abs), abs_top] }
+
+	return B
