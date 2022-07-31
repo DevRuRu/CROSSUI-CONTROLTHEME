c122e2145965559b09037115fe8632c729ea2d75
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2022-07-31 21:47:59 +0000

    Initial implementation.

--- big_int_str_from.v
+++ big_int_str_from.v
@@ -8,4 +8,4 @@ pub fn from_str(str string) ?BigInteger {
 	char_code_comma := ','[0]
 	char_code_minus := '-'[0]
 	mut digits := []byte{}
-	negative := if str[0] == char_co
+	negative := if str[0] == char_code_minus { t
