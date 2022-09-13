68d7bf7c01f676e98d8255d15c17fbbf276002c8
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2022-09-13 21:59:37 +0000

    automated commit from rosetta for sim/lib joist, locale pl

--- big_int_str_from.v
+++ big_int_str_from.v
@@ -26,4 +26,5 @@ pub fn from_str(str string) ?BigInteger {
 	for i := digits.len - 1; i >= 0; i-- {
 		d := digits[i]
 		if negative {
-			num = num - from_int(d) 
+			num = num - from_int(d) * times
+		} else {
