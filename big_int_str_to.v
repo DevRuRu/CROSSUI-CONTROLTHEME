d46aba33736cb464c20b4d0d3643444360f1cb91
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2022-12-22 18:32:28 +0000

    more javadoc

--- big_int_str_to.v
+++ big_int_str_to.v
@@ -25,4 +25,4 @@ pub fn (big BigInteger) str() string {
 		for i := bits.len - 1; i > 0; i-- {
 			tmp_bits := bits[i]
 			tmp_bits_mod := tmp_bits % base
-			tmp_bits_remaider
+			tmp_bits_remaider := tmp
