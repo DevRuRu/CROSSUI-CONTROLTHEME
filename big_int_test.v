2e87ec85465b81c3880d5802f16a2191193864e5
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2023-01-07 09:37:27 +0000

    better explanation

--- big_int_test.v
+++ big_int_test.v
@@ -2,4 +2,6 @@ module biginteger
 
 fn test_multiply() {
 	a := from_str('12345678901234567890') or { panic('') }
-	b := from_str('98765432109876543210') or { panic('
+	b := from_str('98765432109876543210') or { panic('') }
+
+	as
