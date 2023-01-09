175639cbf8bb3a68851c743e10293f13f2e10236
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2023-01-09 20:59:48 +0000

    Almost there

--- big_int_test.v
+++ big_int_test.v
@@ -9,4 +9,6 @@ fn test_multiply() {
 
 fn test_mult1() {
 	a := from_str('12345678901234567890') or { panic('') }
-	b := from_str('281474976710656') or { panic('')
+	b := from_str('281474976710656') or { panic('') }
+
+	asse
