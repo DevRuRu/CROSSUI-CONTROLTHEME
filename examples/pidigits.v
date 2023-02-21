fddc601fda7304f5032bcbb38677c32a4f687219
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2023-02-21 10:31:34 +0000

    nightly build

--- examples/pidigits.v
+++ examples/pidigits.v
@@ -4,3 +4,13 @@ module main
 import os
 import strconv
 import hanabi1224.biginteger
+import strings
+
+fn main() {
+	mut n := 1000
+	if os.args.len > 1 {
+		n = strconv.atoi(os.args[1]) or { n }
+	}
+
+	digits_to_print := n
+	mut digits_printed := 0
