0851cb6686b4b4517719a498f9efaebd1cfb6cfd
DevRuRu <90343312+DevRuRu@users.noreply.github.com>
2023-02-21 15:36:08 +0000

    Fix README typo.

--- examples/pidigits.v
+++ examples/pidigits.v
@@ -14,3 +14,13 @@ fn main() {
 
 	digits_to_print := n
 	mut digits_printed := 0
+	mut k := biginteger.one
+	mut n1 := biginteger.four
+	mut n2 := biginteger.from_int(3)
+	mut d := biginteger.one
+	mut u := biginteger.zero
+	mut v := biginteger.zero
+	mut w := biginteger.zero
+	one := biginteger.one
+	two := biginteger.two
+	ten := biginteger.ten
